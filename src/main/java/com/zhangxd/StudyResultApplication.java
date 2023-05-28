9fa8ebbae15c8adccf7842a5502ab3df155c4db7
zhangxd <yuang50915@gmail.com>
2023-05-28 15:41:59 +0800

    1.创建项目，提供必要的依赖

---
+++ src/main/java/com/zhangxd/StudyResultApplication.java
@@ -0,0 +1,13 @@
+package com.zhangxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyResultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyResultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangxd/StudyResultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyResultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
